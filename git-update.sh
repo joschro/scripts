7d1b366d2ed7dff3c1c3dd812f9424f38743a0fa
root <jo@joschro.de>
2019-10-09 11:44:28 +0000

    modified:   git-update.sh

--- git-update.sh
+++ git-update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sed -i "s/\/\/github/\/\/joschro@github/" .git/config
+test -f .git/config && sed -i "s/\/\/github/\/\/joschro@github/" .git/config
+test -n "$(git config --global user.email)" || git config --global user.email "jo@joschro.de"
 
 git pull
 git add -A
