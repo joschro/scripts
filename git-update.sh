6220bfcf3d81304ea2abdd813485fe0922e67ba5
Joachim Schröder <jschrode@redhat.com>
2022-01-14 14:51:58 +0100

    Update git-update.sh

--- git-update.sh
+++ git-update.sh
@@ -3,6 +3,15 @@
 test -f .git/config && sed -i "s/\/\/github/\/\/joschro@github/" .git/config
 test -n "$(git config --global user.email)" || git config --global user.email "jo@joschro.de"
 
+rpm -q gh || {
+  dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+  dnf install gh -y
+}
+gh auth status || {
+  echo -n "Please enter your GitHub access token: "; read ANSW
+  echo "$ANSW" | gh auth login --with-token
+}
+
 git pull
 git add -A
 git commit -a
