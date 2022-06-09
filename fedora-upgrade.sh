ac05b6c09a87072e897cfb4aa500dcb4165504fb
Joachim Schröder <jschrode@redhat.com>
2022-06-09 11:14:44 +0200

    Add files via upload

---
+++ fedora-upgrade.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+logFile="/var/log/$(basename $0)"
+currentVersion=$(sed "s/.* \([[:digit:]]*\)\( .*\)/\1/g" /etc/fedora-release)
+(( newVersion = $currentVersion + 1 ))
+
+echo;echo "$(date) - Preparing for update $currentVersion -> $newVersion" | tee -a $logFile;echo
+
+dnf clean all | tee -a $logFile
+doReboot="y"
+dnf check-upgrade | tee -a $logFile 
+dnf check-upgrade && doReboot="n"
+dnf -y upgrade --refresh | tee -a $logFile
+test "$doReboot" = "y" && reboot
+dnf -y install dnf-plugin-system-upgrade | tee -a $logFile
+echo;echo "--------------------";echo "$(date) - Update $currentVersion -> $newVersion" | tee -a $logFile;echo
+dnf -y system-upgrade download --releasever=$newVersion | tee -a $logFile
+dnf system-upgrade reboot
