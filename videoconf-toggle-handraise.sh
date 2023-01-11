d170df15592fbf9a99d4b8b27396687b73194d58
Joachim Schröder <jo@joschro.de>
2023-01-11 22:57:36 +0100

    geändert:       videoconf-toggle-handraise.sh

--- videoconf-toggle-handraise.sh
+++ videoconf-toggle-handraise.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-xdotool windowactivate "$(xdotool search --name "Meet -" | head -1)" key Ctrl+Alt+h
-xdotool windowactivate "$(xdotool search --name "BigBlueButton -" | head -1)" key Alt+r
+xdotool windowactivate "$(xdotool search --name "Meet -" | head -1)" key Ctrl+Alt+h || wtype -M ctrl -M alt h -m ctrl -m alt
+xdotool windowactivate "$(xdotool search --name "BigBlueButton -" | head -1)" key Alt+r || wtype -M alt r -m alt
