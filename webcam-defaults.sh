fd1a29721d4818b519062b26ded33ac350e74b66
Joachim Schröder <jo@joschro.de>
2023-04-12 11:22:03 +0200

    modified:   webcam-defaults.sh

--- webcam-defaults.sh
+++ webcam-defaults.sh
@@ -3,17 +3,17 @@
 for I in /dev/video*; do
        v4l2-ctl -D -d $I | grep C920 && {
 	       echo "Webcam found at: $I"
-	       guvcview -z -d $I -p ~/guvcview-videoconf-c920.gpfl
+	       guvcview -z -d $I -p ~/configs/guvcview-videoconf-c920.gpfl
 	       break
        }
        v4l2-ctl -D -d $I | grep C930e && {
 	       echo "Webcam found at: $I"
-	       guvcview -z -d $I -p ~/guvcview-videoconf-c930e.gpfl
+	       guvcview -z -d $I -p ~/configs/guvcview-videoconf-c930e.gpfl
 	       break
        }
        v4l2-ctl -D -d $I | grep Trust && {
 	       echo "Webcam found at: $I"
-	       guvcview -z -d $I -p ~/guvcview-videoconf-trust.gpfl
+	       guvcview -z -d $I -p ~/configs/guvcview-videoconf-trust.gpfl
 	       break
        }
 done
