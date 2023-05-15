dbc5f79562dec2f57da71fd805d2eccf236e5af9
Joachim Schröder <jo@joschro.de>
2023-05-15 10:09:46 +0200

    geändert:       battery_status.sh

--- battery_status.sh
+++ battery_status.sh
@@ -3,5 +3,5 @@
 for I in /sys/class/power_supply/BAT*; do
 	echo -n "Capacity currently against last full $I: "
 	echo -n "$(echo "( 100 * $(cat $I/energy_now) / $(cat $I/energy_full) )" | bc)%"
-	echo " - $(cat $I/status)"
+	echo " - $(cat $I/status) - Power connected: $(cat /sys/class/power_supply/AC/online)"
 done
