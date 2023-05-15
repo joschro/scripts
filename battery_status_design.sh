9e07f517713e7ccdb24a5cf16577086abb8a94c5
Joachim Schröder <jo@joschro.de>
2023-05-15 10:10:57 +0200

    geändert:       battery_status_design.sh 	geändert:       battery_status_lastfull.sh

--- battery_status_design.sh
+++ battery_status_design.sh
@@ -4,5 +4,5 @@ for I in /sys/class/power_supply/BAT*; do
 	battCap="$(echo "100 * $(cat $I/energy_full) / $(cat $I/energy_full_design)" | bc)"
 	echo -n "Capacity last full against design $I: "
 	echo -n "$battCap%"
-	echo " - $(cat $I/status)"
+	echo " - $(cat $I/status) - Power connected: $(cat /sys/class/power_supply/AC/online)"
 done
--- battery_status_lastfull.sh
+++ battery_status_lastfull.sh
@@ -4,5 +4,5 @@ for I in /sys/class/power_supply/BAT*; do
 	battCap="$(echo "100 * $(cat $I/energy_now) / $(cat $I/energy_full)" | bc)"
 	echo -n "Capacity currently against last full $I: "
 	echo -n "$battCap%"
-	echo " - $(cat $I/status)"
+	echo " - $(cat $I/status) - Power connected: $(cat /sys/class/power_supply/AC/online)"
 done
