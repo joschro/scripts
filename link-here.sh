e2bc791f0af254a8a673c9c1483653533cfefadc
Joachim Schröder <jo@joschro.de>
2023-06-16 14:46:15 +0200

    modified:   link-here.sh

--- link-here.sh
+++ link-here.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ln -s ~/Proje*/github/scripts/*sh .
-ln -s ~/Proje*/github/scripts/*py .
+ln -s ~/Proje*/github/scripts/*sh . 2>&1 | grep -v "exist"
+ln -s ~/Proje*/github/scripts/*py . 2>&1 | grep -v "exist"
 ln -snf `ls -rt Logseq-linux-x64-* | tail -n1` Logseq-linux-x64 && chmod u+x ./Logseq-linux-x64
