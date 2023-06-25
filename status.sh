a14e70dbe62e24d145dbb45db91f4a4f4cea37f9
Joachim Schröder <jo@joschro.de>
2023-06-25 14:37:37 +0200

    neue Datei:     status.sh

---
+++ status.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Temperatures"
+paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
+
+echo "Battery"
+battery_status.sh
