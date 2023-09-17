265261b4c34aadb8b99304855624377ef7c3823c
Joachim Schröder <jo@joschro.de>
2023-09-17 20:46:53 +0200

    geändert:       status.sh

--- status.sh
+++ status.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-echo "Temperatures"
+echo;echo "Temperatures"
 paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
 
-echo "Battery"
+echo;echo -n "CPU turbo deactived?: "; cat /sys/devices/system/cpu/intel_pstate/no_turbo
+test $(cat /sys/devices/system/cpu/intel_pstate/no_turbo) -eq 0 && {
+	echo -n "Do you want to deactivate cpu turbo mode [y|n]? "; read ANSW
+	test "$ANSW" = "y" && sudo sh -c "echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo"
+	echo -n "CPU turbo deactived?: "; cat /sys/devices/system/cpu/intel_pstate/no_turbo
+}
+
+echo;echo "Battery"
 battery_status.sh
