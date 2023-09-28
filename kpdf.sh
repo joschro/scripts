eadedafffa00a2c2f3d92254e4ec1f802f15243f
Joachim Schröder <jo@joschro.de>
2023-09-28 17:41:36 +0200

    geändert:       kpdf.sh

--- kpdf.sh
+++ kpdf.sh
@@ -3,4 +3,6 @@
 fileList=""
 for I in $*; do echo "$I"; test -f "$I" && fileList="$fileList $I"; done
 
-qpdfview-qt5 $fileList
+for I in qpdfview-qt6 qpdfview-qt5; do
+	test -f /usr/bin/$I && $I $fileList
+done
