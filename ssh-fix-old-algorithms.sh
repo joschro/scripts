b8d1c1cdcfc11b72a376e992ff652ab19719c796
Joachim Schröder <jo@joschro.de>
2023-10-01 13:07:53 +0200

    geändert:       ssh-fix-old-algorithms.sh

--- ssh-fix-old-algorithms.sh
+++ ssh-fix-old-algorithms.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-sudo echo "HostKeyAlgorithms +ssh-rsa
-PubkeyAcceptedAlgorithms +ssh-rsa" >> /etc/ssh/ssh_config
+grep 'PubkeyAcceptedAlgorithms +ssh-rsa' /etc/ssh/ssh_config || sudo sh -c "echo -e 'HostKeyAlgorithms +ssh-rsa\nPubkeyAcceptedAlgorithms +ssh-rsa' >> /etc/ssh/ssh_config"
 
 echo
 echo "Use: ssh -o KexAlgorithms=diffie-hellman-group1-sha1 -o HostKeyAlgorithms=+ssh-rsa -o Ciphers=+aes256-cbc user@ip"
