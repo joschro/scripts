74c3705d1e16dac89dc71b9ffa6a43dcce828956
Joachim Schröder <jo@joschro.de>
2023-11-01 16:25:07 +0100

    geändert:       battery_status.sh 	geändert:       battery_status_design.sh 	geändert:       battery_status_lastfull.sh

--- battery_status_design.sh
+++ battery_status_design.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 for I in /sys/class/power_supply/BAT*; do
-	battCap="$(echo "100 * $(cat $I/energy_full) / $(cat $I/energy_full_design)" | bc)"
+	test -f $I/charge_full && battCap="$(echo "100 * $(cat $I/charge_full) / $(cat $I/charge_full_design)" | bc)"
+	test -f $I/energy_full && battCap="$(echo "100 * $(cat $I/energy_full) / $(cat $I/energy_full_design)" | bc)"
 	echo -n "Capacity last full against design $I: "
 	echo -n "$battCap%"
 	echo " - $(cat $I/status) - Power connected: $(cat /sys/class/power_supply/AC/online)"
--- battery_status_lastfull.sh
+++ battery_status_lastfull.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 for I in /sys/class/power_supply/BAT*; do
-	battCap="$(echo "100 * $(cat $I/energy_now) / $(cat $I/energy_full)" | bc)"
+	test -f $I/charge_full && battCap="$(echo "100 * $(cat $I/charge_now) / $(cat $I/charge_full)" | bc)"
+	test -f $I/energy_full && battCap="$(echo "100 * $(cat $I/energy_now) / $(cat $I/energy_full)" | bc)"
 	echo -n "Capacity currently against last full $I: "
 	echo -n "$battCap%"
 	echo " - $(cat $I/status) - Power connected: $(cat /sys/class/power_supply/AC/online)"
