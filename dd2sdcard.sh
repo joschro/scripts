d742fff43d0db4cdee8f54ffe2d0d326a3ba5017
Joachim Schröder <jo@joschro.de>
2023-11-09 19:23:41 +0100

    geändert:       dd2sdcard.sh

--- dd2sdcard.sh
+++ dd2sdcard.sh
@@ -2,6 +2,14 @@
 
 myDEV=/dev/sda
 
+myFDISK="sudo fdisk"
+myDD="sudo dd"
+
+test "$USER" = "root" && {
+	myFDISK="fdisk"
+	myDD="dd"
+}
+
 test $# -gt 1 && myDEV=$2
 
 test -f $1 || {
@@ -28,42 +36,42 @@ echo "$1" | grep "\.zip$" && unzip -l "$1"
 
 
 echo; echo "to:"
-fdisk -l $myDEV
+$myFDISK -l $myDEV
 echo;echo "Do you really want to write to this device $myDEV? Cancel with <CTRL-C>..."; read ANSW
 
 echo "$1" | grep "\.tar\.bz2$" && {
 	echo "tar.bz2 detected"
-	tar -xvjf "$1" -o | dd status=progress bs=4M of=$myDEV && sync;sync;sync
+	tar -xvjf "$1" -o | $myDD status=progress bs=4M of=$myDEV && sync;sync;sync
 	exit 0
 }
 echo "$1" | grep "\.tar\.gz$" && {
 	echo "tar.gz detected"
-	tar -xvzf "$1" -o | dd status=progress bs=4M of=$myDEV && sync;sync;sync
+	tar -xvzf "$1" -o | $myDD status=progress bs=4M of=$myDEV && sync;sync;sync
 	exit 0
 }
 echo "$1" | grep "\.bz2$" && {
 	echo "bz2 detected"
-	bunzip2 -dv "$1" -c | dd status=progress bs=4M of=$myDEV && sync;sync;sync
+	bunzip2 -dv "$1" -c | $myDD status=progress bs=4M of=$myDEV && sync;sync;sync
 	exit 0
 }
 echo "$1" | grep "\.gz$" && {
 	echo "gunzip detected"
-	gunzip -c "$1" | dd status=progress bs=4M of=$myDEV && sync;sync;sync
+	gunzip -c "$1" | $myDD status=progress bs=4M of=$myDEV && sync;sync;sync
 	exit 0
 }
 echo "$1" | grep "\.xz$" && {
 	echo "xz detected"
-	xzcat "$1" | dd status=progress bs=4M of=$myDEV && sync;sync;sync
+	xzcat "$1" | $myDD status=progress bs=4M of=$myDEV && sync;sync;sync
 	exit 0
 }
 echo "$1" | grep "\.zip$" && {
 	echo "zip detected"
-	unzip -p "$1" | dd status=progress bs=4M of=$myDEV && sync;sync;sync
+	unzip -p "$1" | $myDD status=progress bs=4M of=$myDEV && sync;sync;sync
 	exit 0
 }
 echo "$1" | grep "\.img$\|\.iso$" && {
 	echo "raw image detected"
-	dd status=progress bs=4M conv=fdatasync if="$1" of=$myDEV && sync;sync;sync
+	$myDD status=progress bs=4M conv=fdatasync if="$1" of=$myDEV && sync;sync;sync
 	exit 0
 }
 
