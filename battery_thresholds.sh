821948beaa64244c8e9f81fc2abb629b47a7e3cc
Joachim Schröder <jo@joschro.de>
2023-11-09 09:29:03 +0100

    geändert:       battery_status.sh 	geändert:       battery_status_design.sh 	geändert:       battery_status_lastfull.sh 	geändert:       battery_thresholds.sh

--- battery_thresholds.sh
+++ battery_thresholds.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-for I in /sys/class/power_supply/BAT0/charge_*; do
-       echo -n "$I: "
-       cat $I
+for J in /sys/class/power_supply/BAT*; do
+	echo "$J ($(cat $J/model_name)):" 
+	for I in $J/charge_*; do
+		echo -ne "\t$I: "
+		cat $I
+	done
 done
