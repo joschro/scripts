d6d8afeadeeda964fd6ec79066dd61df76b4c9aa
Joachim Schröder <jo@joschro.de>
2024-02-03 11:31:07 +0100

    geändert:       fritz-repeater-start-radio.sh

--- fritz-repeater-start-radio.sh
+++ fritz-repeater-start-radio.sh
@@ -5,4 +5,8 @@ test $# -lt 1 && {
 	exit
 }
 fritzPassword="$1"
-curl http://fritz.repeater/cgi-bin/webcm  --data "configd:settings/NLR/PlayControl=1" --data "login:command/password=$fritzPassword" --silent --connect-timeout 1 >/dev/null
+
+test $# -gt 1 && fritzStation="$2" && echo "Station $fritzStation selected."
+
+fritzURL="fritz.repeater"
+ping -q -c1 -w1 $fritzURL >/dev/null && echo "$fritzURL reachable" && curl http://fritz.repeater/cgi-bin/webcm  --data "configd:settings/NLR/PlayControl=$fritzStation" --data "login:command/password=$fritzPassword" --silent --connect-timeout 1 >/dev/null && echo "Turned on radio."
