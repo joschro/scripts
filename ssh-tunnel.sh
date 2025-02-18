7d529b3feeee877ae10ac22b1d11710776199f59
Joachim Schröder <jo@joschro.de>
2025-02-18 01:03:13 +0100

    neue Datei:     ssh-tunnel.sh

---
+++ ssh-tunnel.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+### make sure that on the remote side there "grep GatewayPort /etc/ssh/sshd_config" results in:
+# #GatewayPorts no
+# GatewayPorts yes
+
+myDest="$1"
+ssh $myDest sudo grep GatewayPort /etc/ssh/sshd_config | grep yes || exit
+shift
+myPort="55522"
+test $# -gt 0 && myPort="$1"
+
+ssh -R 0.0.0.0:$myPort:127.0.0.1:22 -N $myDest 
