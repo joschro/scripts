252060309fddbd579155ff178416c3d0f4c4828b
Joachim Schröder <jo@joschro.de>
2025-04-17 19:39:06 +0200

    modified:   sunhours-forecast.sh

--- sunhours-forecast.sh
+++ sunhours-forecast.sh
@@ -3,16 +3,30 @@
 LAT="$(cat ~/Projekte/github/private/location.lat)"
 LON="$(cat ~/Projekte/github/private/location.lon)"
 TOPIC="$(cat ~/Projekte/github/private/ntfy_info.topic)"
+DAY="$1"
 
+case $DAY in 
+	"heute")
+		dayNumber=0;
+		;;
+	"morgen")
+		dayNumber=1;
+		;;
+	"übermorgen")
+		dayNumber=2;
+		;;
+	*)
+		exit
+esac
 
 # Wetterdaten abrufen
 RESPONSE=$(curl -s "https://api.open-meteo.com/v1/forecast?latitude=$LAT&longitude=$LON&daily=sunshine_duration&timezone=Europe/Berlin")
 
 # Sonnenstunden für morgen extrahieren
-SUN_MINS=$(echo $RESPONSE | jq '.daily.sunshine_duration[1]')
+SUN_MINS=$(echo $RESPONSE | jq ".daily.sunshine_duration[$dayNumber]")
 SUN_HOURS="$(echo $SUN_MINS / 3600 | bc)"
 
 # Nachricht an ntfy.sh senden
-MESSAGE="Sonnenstunden morgen: $SUN_HOURS Minuten ☀️"
+MESSAGE="Sonnenstunden $DAY: $SUN_HOURS Stunden ☀️"
 curl -d "$MESSAGE" "https://ntfy.sh/$TOPIC"
 
