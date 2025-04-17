20bbd4b8d5796bd0e583a4ec08da16976430ce42
Joachim Schröder <jo@joschro.de>
2025-04-17 20:15:47 +0200

    gelöscht:       sunhours-forecast-7d.sh 	geändert:       sunhours-forecast.sh

--- sunhours-forecast.sh
+++ sunhours-forecast.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+test "$1" = "-nontfy" && {
+	noNtfy=true
+	shift
+}
+
 LAT="$(cat ~/Projekte/github/private/location.lat)"
 LON="$(cat ~/Projekte/github/private/location.lon)"
 TOPIC="$(cat ~/Projekte/github/private/ntfy_info.topic)"
 DAY="$1"
 
 case $DAY in 
-	"heute")
+	"today"|"heute")
 		dayNumber=0;
 		;;
-	"morgen")
+	"tomorrow"|"morgen")
 		dayNumber=1;
 		;;
-	"übermorgen")
+	"aftertomorrow"|"übermorgen")
 		dayNumber=2;
+		;;
+	"week"|"woche")
+		dayNumber="$(echo {0..6})";
 		;;
 	*)
 		exit
@@ -22,10 +30,14 @@ esac
 # Wetterdaten abrufen
 RESPONSE=$(curl -s "https://api.open-meteo.com/v1/forecast?latitude=$LAT&longitude=$LON&daily=sunshine_duration&timezone=Europe/Berlin")
 
-# Sonnenstunden für morgen extrahieren
-SUN_MINS=$(echo $RESPONSE | jq ".daily.sunshine_duration[$dayNumber]")
-SUN_HOURS="$(echo $SUN_MINS / 3600 | bc)"
+for I in $dayNumber; do
+        # Sonnenstunden für extrahieren
+        SUN_DAY=$(echo $RESPONSE | jq ".daily.time[$I]")
+        SUN_MINS=$(echo $RESPONSE | jq ".daily.sunshine_duration[$I]")
+        SUN_HOURS="$(echo $SUN_MINS / 3600 | bc)"
 
+	MESSAGE="$(echo -e "$MESSAGE\nSonnenstunden $SUN_DAY: $SUN_HOURS Stunden ☀️")"
+done
 # Nachricht an ntfy.sh senden
-MESSAGE="Sonnenstunden $DAY: $SUN_HOURS Stunden ☀️"
-curl -d "$MESSAGE" "https://ntfy.sh/$TOPIC"
+test $noNtfy || curl -s -d "$MESSAGE" "https://ntfy.sh/$TOPIC" >/dev/null
+echo "$MESSAGE"
