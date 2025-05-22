b7422cc21853b81e7ed74ffbe36812e4451321a1
Joachim Schröder <jo@joschro.de>
2025-05-22 16:03:42 +0200

    gelöscht:       kpdf.sh

--- kpdf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-fileList=""
-for I in $*; do echo "$I"; test -f "$I" && fileList="$fileList $I"; done
-
-for I in qpdfview-qt6 qpdfview-qt5; do
-	test -f /usr/bin/$I && $I $fileList
-done
