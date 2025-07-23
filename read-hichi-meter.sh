9d8a7859ed5874efe8745c3bf383cc7f9803681c
Joachim Schröder <jo@joschro.de>
2025-07-23 21:12:26 +0200

    new file:   read-hichi-meter.sh

---
+++ read-hichi-meter.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+test $# -lt 2 && {
+	echo "Syntax: $0 <topic> <hichi ip>"
+        exit
+}
+
+myTopic="$1"
+shift
+
+myMessage="$(echo -e "total_in: $(curl -s "http://$1/cm?cmnd=status%2010" | jq '.StatusSNS."".total_in')\ntotal_out: $(curl -s "http://$1/cm?cmnd=status%2010" | jq '.StatusSNS."".total_out')")"
+
+/root/ntfy.sh "$myTopic" "Stromzählerstände Hichi" "$myMessage"
