b88e00b090f1186929134bbcc752c53132b538d1
Joachim Schröder <jo@joschro.de>
2025-10-05 01:09:53 +0200

    modified:   sonnenbatterie.sh

--- sonnenbatterie.sh
+++ sonnenbatterie.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-test $# -lt 1 && { echo -e "Usage: $0 <path-to-config> [--ip <IP>] [--token <API-Token>] <laden|auto|entlade_stop|entlade_ok|status> [duration(minutes)]"; exit; }
+test $# -lt 1 && { echo -e "Usage: $0 <path-to-config> [--ip <IP>] [--token <API-Token>] [--duration <duration(minutes)>] [--until <percent>] <laden|auto|entlade_stop|entlade_ok|status>"; exit; }
 sonnenBattIP="192.168.178.116"
 sonnenBattAPIUrl="http://$sonnenBattIP:80/api/v2"
 sonnenBattAPIToken="Auth-Token: $(cat $1/api_keys/sonnenbatterie_api_key.txt)"
 chargingPower=4600
 ntfyTopic="$(cat $1/ntfy_info.topic)"
+myDuration=0
+myLoadLimit=100
+cap100=10642
+cap5=1300
 shift
 
 test $# -lt 1 && { echo "Parameter missing. Exiting."; exit;}
@@ -24,6 +28,18 @@ while [ $# -gt 1 ]; do
                         sonnenBattAPIToken="Auth-Token: $1";
 			shift;
                         ;;
+		"--duration")
+                        shift;
+                        test "$#" -lt 1 && { echo "Missing parameter, exiting."; exit;};
+                        myDuration="$1";
+                        shift;
+                        ;;
+		"--until")
+                        shift;
+                        test "$#" -lt 1 && { echo "Missing parameter, exiting."; exit;};
+                        myLoadLimit="$1";
+                        shift;
+                        ;;
 		*)
 			echo "Using default params.";
 			continue;
@@ -39,40 +55,50 @@ case $1 in
 			echo;
 			curl -X POST --header "$sonnenBattAPIToken" -d "" $sonnenBattAPIUrl/setpoint/charge/$chargingPower;
 			echo;
-			myDuration=0; test $# -gt 1 && myDuration="$2"
-			test $myDuration -eq 0 && ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now charging with ${chargingPower}W: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge")";
+			test $myDuration -eq 0 && test $myLoadLimit -eq 100 && ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now charging with ${chargingPower}W: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge") RemainingCapacity_%:$(echo "scale=14; 100 + ($(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh" | sed "s/\"RemainingCapacity_Wh\"://g") / 2 - $cap100) * 95 / ($cap100 - $cap5)" | bc -l | xargs printf "%.0f\n")%";
 			test $myDuration -gt 0 && {
-				ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now charging with ${chargingPower}W for $myDuration minutes: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge")"
-				sleep $(echo $myDuration * 60 | bc -l)
+				ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now charging with ${chargingPower}W for $myDuration minutes: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge") RemainingCapacity_%:$(echo "scale=14; 100 + ($(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh" | sed "s/\"RemainingCapacity_Wh\"://g") / 2 - $cap100) * 95 / ($cap100 - $cap5)" | bc -l | xargs printf "%.0f\n")%"
+				sleep $(echo "$myDuration * 60" | bc -l)
 				curl -X PUT -d EM_OperatingMode=2 --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/configurations
 	                        echo
-        	                ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now in auto mode: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge")"
+        	                ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now in auto mode: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge") RemainingCapacity_%:$(echo "scale=14; 100 + ($(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh" | sed "s/\"RemainingCapacity_Wh\"://g") / 2 - $cap100) * 95 / ($cap100 - $cap5)" | bc -l | xargs printf "%.0f\n")%"
+			};
+			test $myLoadLimit -lt 100 && {
+				ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now charging with ${chargingPower}W until ${myLoadLimit}%: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge") RemainingCapacity_%:$(echo "scale=14; 100 + ($(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh" | sed "s/\"RemainingCapacity_Wh\"://g") / 2 - $cap100) * 95 / ($cap100 - $cap5)" | bc -l | xargs printf "%.0f\n")%"
+				until [ $(echo "scale=14; 100 + ($(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh" | sed "s/\"RemainingCapacity_Wh\"://g") / 2 - $cap100) * 95 / ($cap100 - $cap5)" | bc -l | xargs printf "%.0f\n") -ge $myLoadLimit ]; do
+					sleep 60
+				done
+				curl -X PUT -d EM_OperatingMode=2 --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/configurations
+	                        echo
+        	                ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now in auto mode: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge") RemainingCapacity_%:$(echo "scale=14; 100 + ($(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh" | sed "s/\"RemainingCapacity_Wh\"://g") / 2 - $cap100) * 95 / ($cap100 - $cap5)" | bc -l | xargs printf "%.0f\n")%"
 			};
 			;;
 	"auto")
 			curl -X PUT -d EM_OperatingMode=2 --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/configurations;
 			echo;
-			ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now in auto mode: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge")";
+			ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now in auto mode: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge") RemainingCapacity_%:$(echo "scale=14; 100 + ($(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh" | sed "s/\"RemainingCapacity_Wh\"://g") / 2 - $cap100) * 95 / ($cap100 - $cap5)" | bc -l | xargs printf "%.0f\n")%";
 			;;
 	"entlade_stop")
 			curl -X PUT -d EM_OperatingMode=1 --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/configurations;
                         echo;
 			curl -X POST --header "$sonnenBattAPIToken" -d "" $sonnenBattAPIUrl/setpoint/discharge/0;
 			echo;
-			ntfy.sh "$ntfyTopic" "Sonnenbatterie state changed" "SonnenBatterie discharging stopped: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge")";
+			ntfy.sh "$ntfyTopic" "Sonnenbatterie state changed" "SonnenBatterie discharging stopped: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge") RemainingCapacity_%:$(echo "scale=14; 100 + ($(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh" | sed "s/\"RemainingCapacity_Wh\"://g") / 2 - $cap100) * 95 / ($cap100 - $cap5)" | bc -l | xargs printf "%.0f\n")%";
 			;;
 	"entlade_ok")
 			curl -X PUT -d EM_OperatingMode=2 --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/configurations;
 			echo;
 			curl -X POST --header "$sonnenBattAPIToken" -d "" $sonnenBattAPIUrl/setpoint/discharge/$chargingPower;
 			echo;
-			ntfy.sh "$ntfyTopic" "Sonnenbatterie state changed" "SonnenBatterie discharging allowed: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge")";
+			ntfy.sh "$ntfyTopic" "Sonnenbatterie state changed" "SonnenBatterie discharging allowed: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge") RemainingCapacity_%:$(echo "scale=14; 100 + ($(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh" | sed "s/\"RemainingCapacity_Wh\"://g") / 2 - $cap100) * 95 / ($cap100 - $cap5)" | bc -l | xargs printf "%.0f\n")%";
 			;;
 	"status")
-                        ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "$(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "OperatingMode\|RemainingCapacity_Wh\|Pac_total_W\|dischargeNotAllowed\|GridFeedIn_W")";
+                        ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "$(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "OperatingMode\|RemainingCapacity_Wh\|Pac_total_W\|dischargeNotAllowed\|GridFeedIn_W") RemainingCapacity_%:$(echo "scale=14; 100 + ($(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh" | sed "s/\"RemainingCapacity_Wh\"://g") / 2 - $cap100) * 95 / ($cap100 - $cap5)" | bc -l | xargs printf "%.0f\n")%";
+                        #echo "$(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh")";
+			#echo $(echo "scale=14; 100 + ($(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh" | sed "s/\"RemainingCapacity_Wh\"://g") / 2 - $cap100) * 95 / ($cap100 - $cap5)" | bc -l | xargs printf "%.0f\n")%;
                         ;;
 	*)
 			echo "Unknown command: $1";
-			echo -e "Usage: $0 [--ip <IP>] [--token <API-Token>] <laden|auto|entlade_stop|entlade_ok>";
+			echo -e "Usage: $0 <path-to-config> [--ip <IP>] [--token <API-Token>] [--duration <duration(minutes)>] [--until <percent>] <laden|auto|entlade_stop|entlade_ok|status>";
 esac
 echo
