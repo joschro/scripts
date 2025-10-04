d50ea09eff80ad7001bc56c2a0e5039b1149449c
Joachim Schröder <jo@joschro.de>
2025-10-04 23:40:58 +0200

    geändert:       sonnenbatterie.sh

--- sonnenbatterie.sh
+++ sonnenbatterie.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-test $# -lt 1 && { echo -e "Usage: $0 <path-to-config> [--ip <IP>] [--token <API-Token>] <laden|auto|entlade_stop|entlade_ok|status>"; exit; }
+test $# -lt 1 && { echo -e "Usage: $0 <path-to-config> [--ip <IP>] [--token <API-Token>] <laden|auto|entlade_stop|entlade_ok|status> [duration(minutes)]"; exit; }
 sonnenBattIP="192.168.178.116"
 sonnenBattAPIUrl="http://$sonnenBattIP:80/api/v2"
 sonnenBattAPIToken="Auth-Token: $(cat $1/api_keys/sonnenbatterie_api_key.txt)"
@@ -39,7 +39,15 @@ case $1 in
 			echo;
 			curl -X POST --header "$sonnenBattAPIToken" -d "" $sonnenBattAPIUrl/setpoint/charge/$chargingPower;
 			echo;
-			ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now charging with ${chargingPower}W: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge")";
+			myDuration=0; test $# -gt 1 && myDuration="$2"
+			test $myDuration -eq 0 && ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now charging with ${chargingPower}W: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge")";
+			test $myDuration -gt 0 && {
+				ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now charging with ${chargingPower}W for $myDuration minutes: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge")"
+				sleep $(echo $myDuration * 60 | bc -l)
+				curl -X PUT -d EM_OperatingMode=2 --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/configurations
+	                        echo
+        	                ntfy.sh "$ntfyTopic" "Sonnenbatterie status" "SonnenBatterie now in auto mode: $(curl -s --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/status | sed "s/,/\n/g" | grep -i "RemainingCapacity_Wh\|OperatingMode\|discharge")"
+			};
 			;;
 	"auto")
 			curl -X PUT -d EM_OperatingMode=2 --header "$sonnenBattAPIToken" $sonnenBattAPIUrl/configurations;
