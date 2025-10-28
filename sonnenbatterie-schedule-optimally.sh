5fdd0c33d334b134efb8863a987dfd88ab11f7a2
Joachim Schröder <jo@joschro.de>
2025-10-28 18:08:41 +0100

    modified:   sonnenbatterie-schedule-optimally.sh

--- sonnenbatterie-schedule-optimally.sh
+++ sonnenbatterie-schedule-optimally.sh
@@ -4,7 +4,11 @@ test $# -lt 2 && { echo -e "Usage: $0 <path-to-config> <percent>"; exit; }
 
 percentGoal="$2"
 percentNow="$(sonnenbatterie.sh ~/Projekte/github/private --nontfy status | grep RemainingCapacity_% | sed "s/RemainingCapacity_%://g;s/%//g")"
-percentDiff="$(echo "$percentGoal - $percentNow" | bc -l"
+percentDiff="$(echo "$percentGoal - $percentNow" | bc -l)"
 numberOfQuarterlyHours="$(echo "scale=0; $percentDiff / 10" | bc -l)"
 
-echo at "$(sh ~/bin/tibber-fetch-prices.sh $1 $numberOfQuarterlyHours)"
+echo $percentGoal
+echo $percentNow
+echo $percentDiff
+echo $numberOfQuarterlyHours
+at "$(sh ~/bin/tibber-fetch-prices.sh $1 $numberOfQuarterlyHours) <<< \"sh ~/bin/sonnenbatterie.sh ~/Projekte/github/private --until $percentGoal laden\""
